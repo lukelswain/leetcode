446ee8fbec297af9ec1fb742e4e7f64efd573f11
Luke <lukelswain@btinternet.com>
2026-08-18 15:40:38 +0100

    Add alternative use of binary search for 35. with three pointers. Just as fast but uses more memory so worse that the previous overall

--- Easy/35. Search Insert Position.py
+++ Easy/35. Search Insert Position.py
@@ -15,3 +15,17 @@ class Solution:
                 nums = nums[index+1:]
                 continue
         return total_index
+
+class Solution2:
+    def searchInsert(self, nums:list[int], target:int) -> int:
+        first = 0
+        end = len(nums) - 1
+        while first <= end:
+            mid = (first + end) // 2
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                end = mid - 1
+            else:
+                first = mid + 1
+        return first
